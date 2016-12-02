79e27e4d06bd3e1a259c5b0c88a5c1f9e7f22dc7
koshelnik <illya.koshelnik@mail.ru>
2016-12-02 13:57:42 +0200

    zadachka

---
+++ 1 день (42).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+using namespace std;
+int main()
+{setlocale(LC_ALL,("Russian"));
+	char a = 206;
+	cout << a << endl;
+	char w[] = {192, 197, 200, 206, 211, 221, 222, 223};
+	for (int i=-126; i<128; i++)
+	cout<<i<<"  "<<(char)i<<endl;
+	char e[100];
+	cin.get(e,100);
+	int l=strlen(e);
+	for(int i=0;i<l;i++)
+	cout<<e[i];
+	for(int i=0;i<l;i++)
+	{
+	
+	if(e[i]==-64);
+	if(e[i]==-64&&e[i]==-59&&e[i]==-50&&e[i]==-45&&e[i]==-35&&e[i]==-34&&e[i]==-33);
+	cout<<e[i];
+    }
+	system("pause");
+    } 
